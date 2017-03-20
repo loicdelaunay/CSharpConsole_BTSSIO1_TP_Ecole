18d1dd9e422d0ced121a51341dbb4af65202b164
loïc delaunay <loicdelaunay05@gmail.com>
2017-03-20 21:37:58 +0100

    Création de la classe ContactEcole

---
+++ CSharpConsole_BTSSIO1_TP_Ecole/ContactEcole.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsole_BTSSIO1_TP_Ecole
+{
+    class ContactEcole
+    {
+        private int anneeArrivee;
+        private string nom;
+
+        public ContactEcole(int pAnneeArrivee, string pNom)
+        {
+            anneeArrivee = pAnneeArrivee;
+            nom = pNom;
+        }
+
+        public int GetAnneeArrivee()
+        {
+            return anneeArrivee;
+        }
+
+        public string GetNom()
+        {
+            return nom;
+        }
+
+        public override string ToString()
+        {
+            return "Nom : " + nom + "Arrivé le : " + anneeArrivee;
+        }
+    }
+}
